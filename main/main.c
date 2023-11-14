8f08d324ca24d0b5dcbea41f7dbb7f29cea5842f
Alexey Zaytsev <alexej3ajtsev@icloud.com>
2023-11-15 02:12:23 +0600

    chore: typo

--- main/main.c
+++ main/main.c
@@ -3,7 +3,6 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "driver/rmt_tx.h"
-// #include "led_strip_encoder.h"
 #include "led_matrix.h"
 
 #define RMT_LED_STRIP_RESOLUTION_HZ 10000000 // 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
@@ -11,8 +10,6 @@
 
 #define LED_PER_COL                 1
 #define LED_PER_ROW                 4
-#define PIXELS_PER_CELL             3
-#define EXAMPLE_LED_NUMBERS         4 // FIXME: Обязательно выставляем актуальное значение
 #define EXAMPLE_CHASE_SPEED_MS      100
 
 static const char *TAG = "led_matrix";
